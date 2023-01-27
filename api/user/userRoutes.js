1c8bb099452fd066458575e1431acca806df2abe
teamglue-jun <jun@teamglue.io>
2023-01-27 21:51:27 +0900

    feat: setLocation 수정

--- api/user/userRoutes.js
+++ api/user/userRoutes.js
@@ -1,6 +1,6 @@
 const userController = require('./userController');
 const router = require('express').Router();
-const { verifyToken } = require('../../middlewares/auth/token')
+const { verifyToken } = require('../../middlewares/auth/token');
 
 // router.use(verifyToken);
 
@@ -12,6 +12,11 @@ router.patch('/:user_id/location1', userController.updateLocation);
 router.patch('/:user_id/location2', userController.updateLocation);
 router.delete('/:user_id/location1', userController.deleteLocation);
 router.delete('/:user_id/location2', userController.deleteLocation);
+router.get('/:user_id/area1', userController.getArea);
+router.get('/:user_id/area2', userController.getArea);
+router.patch('/:user_id/area1/:area', userController.updateArea);
+router.patch('/:user_id/area2/:area', userController.updateArea);
+router.get('/:user_id/active-location', userController.getActiveLocation);
 router.post('/:user_id/active-location/:active_location', userController.updateActiveLocation);
 router.delete('/:user_id/withdraw', userController.withdraw);
 
