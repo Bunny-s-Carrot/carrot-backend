a9d1229ffb4d328664a9f55d2f523b5f00537425
qkrwhdgh126 <68217732+qkrwhdgh126@users.noreply.github.com>
2023-02-02 00:19:39 +0900

    fix: 댓글 코드 수정

--- api/post/postService.js
+++ api/post/postService.js
@@ -1,3 +1,4 @@
+const { query } = require('../../config/database');
 const pool = require("../../config/database");
 
 const getPosts = async () => {
@@ -72,13 +73,68 @@ const createPost = async (data) => {
 const getComments = async (postId) => {
   try {
     const comments = await pool.query(
-      `select comment_id, name, lowest_sect_name, COMMENT.created_at, comment, likes, depth from COMMENT 
+      `select comment_id, name, lowest_sect_name, COMMENT.created_at, comment, likes, depth, mother_id from COMMENT 
       inner JOIN USER on user_id = writer_id
       inner join LOCATION on location = location_id
-      where post_id = ?`,
+      where post_id = ?
+      order by mother_id`,
       [postId]
     )
     return comments[0]
+  } catch (e) {
+    throw Error(e);
+  }
+}
+
+const createComment = async (data) => {
+  try {
+    const result = await pool.query(
+      `insert into COMMENT(post_id, writer_id, comment, depth)
+      values(?,?,?,0)`,
+      [
+        data.post_id,
+        data.writer_id,
+        data.content
+      ],
+    )
+
+    const result1 = await pool.query(
+      `select comment_id from COMMENT where comment = ?`,
+      [
+        data.content
+      ]
+    )
+
+    const commentId = result1[0][0].comment_id;
+
+    const result2 = await pool.query(
+      `update COMMENT set mother_id = ? where comment_id = ?`,
+      [
+        commentId,
+        commentId
+      ]
+    )
+
+    return result2[0]
+  } catch (e) {
+    throw Error(e);
+  }
+}
+
+const createRecomment = async (data) => {
+  try {
+    const result = await pool.query(
+      `insert into COMMENT(post_id, writer_id, comment, depth, mother_id)
+      values(?,?,?,1,?)`,
+      [
+        data.post_id,
+        data.writer_id,
+        data.content,
+        data.mother_id
+      ],
+    )
+    
+    return result[0]
   } catch (e) {
     throw Error(e);
   }
@@ -89,7 +145,9 @@ const postService = {
   getPostById,
   getPostsByCategory,
   createPost,
-  getComments
+  getComments,
+  createComment,
+  createRecomment
 };
 
 module.exports = postService;
