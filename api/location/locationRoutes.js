b84443f801110a44f6cc8240724a46ee5393db2b
t-junne <tjunnne@gmail.com>
2023-02-02 22:29:16 +0900

    fix: product 지역코드 업데이트 안 되는 오류 수정

--- api/location/locationRoutes.js
+++ api/location/locationRoutes.js
@@ -4,6 +4,6 @@ const router = require('express').Router();
 
 router.get('/list', locationController.getLocations);
 router.get('/:location_id/coords', locationController.getCoordsById);
-router.get('/:location_id/h_code', locationController.getHcodeById);
+router.get('/:location_id/adm_cd', locationController.getHcodeById);
 
 module.exports = router;
