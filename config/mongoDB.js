2f9a67c2548d93df818bdbfbcfeadf03390488bf
qkrwhdgh126 <68217732+qkrwhdgh126@users.noreply.github.com>
2023-02-10 16:03:44 +0900

    feat: mongodb 데이터 추가

--- config/mongoDB.js
+++ config/mongoDB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connect = () => {
     mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@bunnyscarrot.dqtmy5q.mongodb.net/test`, {
-    dbName: 'carrot',
+    dbName: 'carrotDB',
 }, (error) => {
     if (error) {
         console.log('연결 에러')
