0ff5f79d3b904764ef17056b392b55e53e5f4b1b
t-junne <108136143+t-junne@users.noreply.github.com>
2023-02-11 02:06:51 +0900

    feat: webSocket (진행중)

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const cors = require('cors');
 const app = express();
+const http = require('http')
+const cors = require('cors');
+const { Server } = require('socket.io');
 require("dotenv").config();
 const cookieParser = require('cookie-parser');
 const multer = require('multer')
 const mainRouter = require('./routes');
-const connect = require('./config/mongoDB');
-const io = require('socket.io');
+const mongoDBConnect = require('./config/mongoDB');
+
 
 app.use(cors({
   origin: ['https://app.bunnyscarrot.com', 'http://localhost:3000'],
@@ -19,14 +21,26 @@ app.use(express.json());
 
 app.use('/', mainRouter);
 
-connect();
+const server = http.createServer(app);
 
-const server = app.listen(process.env.SERVER_PORT, () => {
+const io = new Server(server, {
+  cors: {
+    origin: ['http://localhost:3000', 'app.bunnyscarrot.com']
+  }
+});
+
+mongoDBConnect();
+
+server.listen(process.env.SERVER_PORT, () => {
   console.log(`listening on PORT: ${process.env.SERVER_PORT}`);
 });
 
-io(server, {
-  cors: {
-    origin: ['https://app.bunnyscarrot.com', 'http://localhost:3000']
-  }
+io.on('connection', socket => {
+  console.log('Connected with Id: ', socket.id);
+
+  socket.on('message', (data) => {
+    console.log(data);
+  })
+
+  socket.emit('message2', "서버에서 보내는 메시지");
 })
